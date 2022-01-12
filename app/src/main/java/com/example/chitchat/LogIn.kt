1b267f621a32c4a97b6caeedb62dec7e46435037
Pragyay <p.ksingh562001@gmail.com>
2022-01-12 16:44:52 +0530

    Added login page

---
+++ app/src/main/java/com/example/chitchat/LogIn.kt
@@ -0,0 +1,37 @@
+package com.example.chitchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class LogIn : AppCompatActivity() {
+
+    private lateinit var edtEmail: EditText
+    private lateinit var edtPassword: EditText
+    private lateinit var btnLogin: Button
+    private lateinit var btnSignup: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        edtEmail = findViewById(R.id.edtEmail)
+        edtPassword = findViewById(R.id.edtPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnSignup = findViewById(R.id.btnSignup)
+
+        // opening sign up activity
+        btnSignup.setOnClickListener{
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        // log in button
+        btnLogin.setOnClickListener{
+            Toast.makeText(applicationContext, "Logging in..",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/example/chitchat/SignUp.kt
+++ app/src/main/java/com/example/chitchat/SignUp.kt
@@ -3,9 +3,9 @@ package com.example.chitchat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class login : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_sign_up)
     }
 }
