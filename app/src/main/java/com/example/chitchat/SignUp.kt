a5ad47f626f3ac5f6d3ebcbb1ab4aa33086f8e50
Pragyay <p.ksingh562001@gmail.com>
2022-01-17 19:05:39 +0530

    minor changes( typos, indentation)

--- app/src/main/java/com/example/chitchat/SignUp.kt
+++ app/src/main/java/com/example/chitchat/SignUp.kt
@@ -1,4 +1,4 @@
-    package com.example.chitchat
+package com.example.chitchat
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
@@ -9,8 +9,7 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
-
-    class SignUp : AppCompatActivity() {
+class SignUp : AppCompatActivity() {
 
     // following items would be initialized later
     private lateinit var edtName: EditText
@@ -34,7 +33,7 @@ import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
         // button to register user
         btnRegister.setOnClickListener{
-            Toast.makeText(applicationContext, "Registering",Toast.LENGTH_SHORT).show()
+//            Toast.makeText(applicationContext, "Registering",Toast.LENGTH_SHORT).show()
             val name = edtName.text.toString()
             val email = edtEmail.text.toString()
             val password = edtPassword.text.toString()
@@ -59,7 +58,6 @@ import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
     }
 
-
     private fun signUp(email: String, password: String) {
         // logic of creating user
         mAuth.createUserWithEmailAndPassword(email, password)
@@ -71,7 +69,7 @@ import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
                 } else {
                     // display error message
-                    Toast.makeText(this@SignUp, "Some error occured", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@SignUp, "Some error occurred", Toast.LENGTH_SHORT).show()
 
                 }
             }
