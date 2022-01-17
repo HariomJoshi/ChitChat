6ca19e07606751cfbfbfe32ab63cc3262c60cf4b
Hariom Joshi <goodhariom@gmail.com>
2022-01-17 15:42:28 +0530

    Sign up functionality added

--- app/src/main/java/com/example/chitchat/SignUp.kt
+++ app/src/main/java/com/example/chitchat/SignUp.kt
@@ -1,10 +1,13 @@
     package com.example.chitchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 
 
     class SignUp : AppCompatActivity() {
@@ -15,10 +18,13 @@ import android.widget.Toast
     private lateinit var edtEmail: EditText
     private lateinit var edtPassword: EditText
     private lateinit var btnRegister: Button
+    private lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        mAuth = FirebaseAuth.getInstance()
 
         edtName = findViewById(R.id.edtName)
         edtAge = findViewById(R.id.edtAge)
@@ -28,8 +34,34 @@ import android.widget.Toast
 
         // button to register user
         btnRegister.setOnClickListener{
-            Toast.makeText(applicationContext, "Registering",Toast.LENGTH_SHORT).show();
+            Toast.makeText(applicationContext, "Registering",Toast.LENGTH_SHORT).show()
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email, password)
         }
+
     }
+
+
+    private fun signUp(email: String, password: String) {
+        // logic of creating user
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // code for jumping to home
+                    val intent = Intent(this@SignUp, MainActivity:: class.java)
+                    startActivity(intent)
+
+                } else {
+                    // display error message
+                    Toast.makeText(this@SignUp, "Some error occured", Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+    }
+
+
 
 }
