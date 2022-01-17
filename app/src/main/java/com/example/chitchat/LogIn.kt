632d0ab858be8e4869b02a0b88fb3ebcbf98eab8
Pragyay <p.ksingh562001@gmail.com>
2022-01-17 14:46:27 +0530

    added login with firebase, changed minSDKVersion to 19

--- app/src/main/java/com/example/chitchat/LogIn.kt
+++ app/src/main/java/com/example/chitchat/LogIn.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LogIn : AppCompatActivity() {
 
@@ -14,9 +15,14 @@ class LogIn : AppCompatActivity() {
     private lateinit var btnLogin: Button
     private lateinit var btnSignup: Button
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // initializing firebase authentication
+        mAuth = FirebaseAuth.getInstance()
 
         edtEmail = findViewById(R.id.edtEmail)
         edtPassword = findViewById(R.id.edtPassword)
@@ -24,14 +30,39 @@ class LogIn : AppCompatActivity() {
         btnSignup = findViewById(R.id.btnRegister)
 
         // opening sign up activity
-        btnSignup.setOnClickListener{
+        btnSignup.setOnClickListener {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
 
         // log in button
-        btnLogin.setOnClickListener{
-            Toast.makeText(applicationContext, "Logging in..",Toast.LENGTH_SHORT).show();
+        btnLogin.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            if (email.trim().isNotEmpty() && password.trim().isNotEmpty())
+                login(email, password)
+            // handling empty text fields
+            else {
+                if (email.trim().isEmpty())
+                    edtEmail.error = "Username cannot be empty"
+                if (password.trim().isEmpty())
+                    edtPassword.error = "Password cannot be empty"
+            }
+        }
+    }
+    private fun login(email: String, password: String){
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // if login successful, go to MainActivity
+                    val intent = Intent(this@LogIn, MainActivity::class.java)
+                    startActivity(intent);
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@LogIn, "User does not exist.", Toast.LENGTH_SHORT).show()
+                }
         }
     }
 }
